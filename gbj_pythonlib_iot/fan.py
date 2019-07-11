38a57792150095bcbc85cd499a3b03e27fb9121c
Libor Gabaj <gabaj.libor@slsp.sk>
2019-07-11 13:26:09 +0200

    Added setters and getters and functionality enhancement.

--- gbj_pythonlib_iot/fan.py
+++ gbj_pythonlib_iot/fan.py
@@ -12,59 +12,140 @@ __email__ = 'libor.gabaj@gmail.com'
 
 import logging
 
-# from . import common as common
+from . import system as iot_system
 
 
 ###############################################################################
 # Constants
 ###############################################################################
+_PERCON_DEF = 90.0
+_PERCON_MIN = 80.0
+_PERCON_MAX = 95.0
+"""float: Default, minimal, and maximal start temperature expressed
+   in percentage of maximal one.
+"""
+
+_PERCOFF_DEF = 60.0
+_PERCOFF_MIN = 50.0
+_PERCOFF_MAX = 75.0
+"""float: Default, minimal, and maximal stop temperature expressed
+   in percentage of maximal one.
+"""
+
+_PIN = ''
 
 
 ###############################################################################
 # Classes
 ###############################################################################
 class Fan(object):
-    """IoT processing the cooling fan."""
+    """IoT processing the cooling fan.
 
-    def __init__(self):
+    Arguments
+    ---------
+    pin : str
+        Microcomputer I/O pin name for controlling a cooling fan.
+    """
+
+    def __init__(self, pin='PA13'):
         """Create the class instance - constructor."""
+        # Utilized IoT devices
+        self._system = iot_system.System()
+        # Set default values
+        self.reset()
+        self.set_status()
+        self.set_pin(pin)
+        # Logging
         self._logger = logging.getLogger(' '.join([__name__, __version__]))
-        self._logger.debug('Instance of %s created', self.__class__.__name__)
-        # Device parameters
-        self.temperature_on = None  # Start value in centigrades
-        self.temperature_off = None  # Stop value in centigrades
-        self.status = None  # Recent status detected
+        self._logger.debug(
+            'Instance of %s created: %s',
+            self.__class__.__name__,
+            str(self)
+            )
 
     def __str__(self):
         """Represent instance object as a string."""
-        return 'CoolingFan'
+        return 'CoolingFan ({})'.format(self.get_pin())
+
+    def reset(self):
+        """Set all the default parameters."""
+        self.set_percentage_on()
+        self.set_percentage_off()
 
     # -------------------------------------------------------------------------
     # Setters
     # -------------------------------------------------------------------------
-    def set_temperature_on(self, temperature):
+    def set_percentage_on(self, percentage=None):
+        """Save start temperature percentage.
+
+        Arguments
+        ---------
+        percentage : float
+            Temperature in percentage of maximal temperature at which the fan
+            starts cooling.
+            If not provided, the default value is used
+
+        """
+        percentage = \
+            max(min(percentage or _PERCON_DEF, _PERCON_MAX), _PERCON_MIN)
+        self._percentage_on = percentage
+        self._temperature_on = \
+            self._system.calculate_temperature_value(self._percentage_on)
+
+    def set_percentage_off(self, percentage=None):
+        """Save stop temperature percentage.
+
+        Arguments
+        ---------
+        percentage : float
+            Temperature in percentage of maximal temperature at which the fan
+            stops cooling.
+            If not provided, the default value is used
+
+        """
+        percentage = \
+            max(min(percentage or _PERCOFF_DEF, _PERCOFF_MAX), _PERCOFF_MIN)
+        self._percentage_off = percentage
+        self._temperature_off = \
+            self._system.calculate_temperature_value(self._percentage_off)
+
+    def set_temperature_on(self, temperature=None):
         """Save start temperature.
 
         Arguments
         ---------
         temperature : float
             Temperature in centigrades at which the fan starts cooling.
+            If not provided the value calculated from default percentage
+            is used.
 
         """
-        self.temperature_on = temperature
+        percentage = None
+        if temperature is not None:
+            percentage = self._system.calculate_temperature_percentage(
+                temperature
+                )
+        self.set_percentage_on(percentage)
 
-    def set_temperature_off(self, temperature):
+    def set_temperature_off(self, temperature=None):
         """Save stop temperature.
 
         Arguments
         ---------
         temperature : float
             Temperature in centigrades at which the fan stops cooling.
+            If not provided the value calculated from default percentage
+            is used.
 
         """
-        self.temperature_off = temperature
+        percentage = None
+        if temperature is not None:
+            percentage = self._system.calculate_temperature_percentage(
+                temperature
+                )
+        self.set_percentage_off(percentage)
 
-    def set_status(self, status):
+    def set_status(self, status=None):
         """Save recently detected status.
 
         Arguments
@@ -73,19 +154,35 @@ class Fan(object):
             Status code normalized.
 
         """
-        self.status = status
+        self._status = status
+
+    def set_pin(self, pin):
+        """Save microcomputer pin name to control a fan."""
+        self._pin = pin
 
     # -------------------------------------------------------------------------
     # Getters
     # -------------------------------------------------------------------------
+    def get_percentage_on(self):
+        """Return start temperature percentage recently saved."""
+        return self._percentage_on
+
+    def get_percentage_off(self):
+        """Return stop temperature percentage recently saved."""
+        return self._percentage_off
+
     def get_temperature_on(self):
         """Return start temperature recently saved."""
-        return self.temperature_on
+        return self._temperature_on
 
     def get_temperature_off(self):
         """Return stop temperature recently saved."""
-        return self.temperature_off
+        return self._temperature_off
 
     def get_status(self):
         """Return status code recently saved."""
-        return self.status
+        return self._status
+
+    def get_pin(self):
+        """Return microcomputer fan control pin name."""
+        return self._pin
--- gbj_pythonlib_iot/system.py
+++ gbj_pythonlib_iot/system.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Module for supporting host microcomputer system."""
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __status__ = 'Beta'
 __author__ = 'Libor Gabaj'
 __copyright__ = 'Copyright 2019, ' + __author__
@@ -11,6 +11,8 @@ __email__ = 'libor.gabaj@gmail.com'
 
 
 import logging
+import platform
+import random
 
 
 ###############################################################################
@@ -21,51 +23,74 @@ class System(object):
 
     def __init__(self):
         """Create the class instance - constructor."""
+        # Cached system maximal temperature
+        self._temperature_max = self.get_temperature_maximal()
+        # Logging
         self._logger = logging.getLogger(' '.join([__name__, __version__]))
-        self._logger.debug('Instance of %s created', self.__class__.__name__)
-        # Device parameters
-        self.temperature_cur = None  # Current value in centigrades
-        self.temperature_max = None  # Maximal value in centigrades
+        self._logger.debug(
+            'Instance of %s created: %s',
+            self.__class__.__name__,
+            str(self)
+            )
 
     def __str__(self):
         """Represent instance object as a string."""
-        return 'Microcomputer'
+        return 'Microcomputer ({}°C)'.format(self._temperature_max)
 
-    # -------------------------------------------------------------------------
-    # Setters
-    # -------------------------------------------------------------------------
-    def set_temperature_current(self, temperature):
-        """Save current temperature.
+    def _read_temperature(self, system_path):
+        """Read system file and interpret the content as the temperature.
 
         Arguments
         ---------
+        system_path : str
+            Full path to a file with system temperature.
+
+        Returns
+        -------
         temperature : float
-            Current temperature expressed in centigrades.
+            System temperature in centigrades Celsius.
+            If some problem occurs with reading system file, the None is
+            provided.
 
         """
-        self.temperature_cur = temperature
-
-    def set_temperature_maximal(self, temperature):
-        """Save maximal temperature.
-
-        Arguments
-        ---------
-        temperature : float
-            Maximal value.
-
-        """
-        self.temperature_max = temperature
+        try:
+            system_file = open(system_path)
+            content = system_file.read()
+            temperature = float(content)
+            # Raspbian with temp in centigrades, other Pis in millicentigrades
+            if temperature > 85.0:
+                temperature /= 1000.0
+            system_file.close()
+        except Exception:
+            temperature = None
+        return temperature
 
     # -------------------------------------------------------------------------
     # Getters
     # -------------------------------------------------------------------------
-    def get_temperature_current(self):
-        """Return current temperature recently saved."""
-        return self.temperature_cur
+    def get_temperature(self):
+        """Read system current temperature."""
+        temperature = self._read_temperature(
+            '/sys/class/thermal/thermal_zone0/temp'
+            )
+        if temperature is None and platform.system() == 'Windows':
+            temperature = float(random.randint(40, 70))
+        return temperature
 
     def get_temperature_maximal(self):
-        """Return maximal temperature recently saved."""
-        return self.temperature_max
+        """Read system maximal temperature."""
+        # Get temperature from the cache
+        if hasattr(self, '_temperature_max') \
+                and self._temperature_max is not None:
+            return self._temperature_max
+        # Read temperature from the system
+        temperature = self._read_temperature(
+            '/sys/class/thermal/thermal_zone0/trip_point_0_temp'
+            )
+        if temperature is None and platform.system() == 'Windows':
+            temperature = 75.0
+        self._temperature_max = temperature  # Cache temperature
+        return temperature
 
     # -------------------------------------------------------------------------
     # Calculations
@@ -87,8 +112,9 @@ class System(object):
 
         """
         percentage = None
-        if self.temperature_max:
-            percentage = temperature / self.temperature_max * 100.0
+        self.get_temperature_maximal()
+        if self._temperature_max:
+            percentage = temperature / self._temperature_max * 100.0
         return percentage
 
     def calculate_temperature_value(self, percentage):
@@ -108,6 +134,7 @@ class System(object):
 
         """
         temperature = None
-        if self.temperature_max:
-            temperature = percentage / 100.0 * self.temperature_max
+        self.get_temperature_maximal()
+        if self._temperature_max:
+            temperature = percentage / 100.0 * self._temperature_max
         return temperature
